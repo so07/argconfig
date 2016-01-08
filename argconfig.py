b1349bea1524ff04743affe86d241d98fd485e9e
s.orlandini <s.orlandini@cineca.it>
2016-01-08 14:46:18 +0100

    - add __author__ and __version__ variables - overwrite_default option - verbose option - remove main from argconfig file - clean __init__.py file

--- argconfig.py
+++ argconfig.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 import os
-import sys
 import inspect
 import argparse
 import ConfigParser
 
-import_file = inspect.stack()[1][1]
+__author__  = 'Sergio Orlandini'
+__version__ = '0.2dev'
+
+try:
+    import_file = inspect.stack()[1][1]
+except:
+    import_file = __file__
 
 class argconfig(argparse.Action):
 
-     config_file = 'config.ini' 
-     config_section = ['default']
-     config_path = [
-                     os.getcwd(),                                         # current work dir
-                     os.path.dirname( os.path.realpath(__file__) ),       # installation dir
-                     os.path.dirname( os.path.realpath( import_file ) ),  # caller's dir
-                   ]
+     _config_file = 'config.ini'
+     _config_section = ['default']
+     _config_path = [
+                      os.getcwd(),                                         # current work dir
+                      os.path.dirname( os.path.realpath(__file__) ),       # installation dir
+                      os.path.dirname( os.path.realpath( import_file ) ),  # caller's dir
+                    ]
 
-     def __init__(self, **kwargs):
+     def __init__(self, overwrite_default=True, verbose=False, **kwargs):
 
          argparse_keys = ['option_strings', 'dest', 'const', 'default', 'type', 'choices', 'required', 'help', 'metavar']
          _d = {'option_strings' : None}
@@ -36,7 +41,7 @@ class argconfig(argparse.Action):
          # default
          _default = self.get_default()
 
-         if _default:
+         if _default and overwrite_default:
              self.default = _default
 
 
@@ -56,7 +61,7 @@ class argconfig(argparse.Action):
 
          update = False
 
-         _section = self.config_section
+         _section = self._config_section
          if hasattr(self, 'section'):
             _section += self.section
 
@@ -88,7 +93,7 @@ class argconfig(argparse.Action):
             Prepend path to default list.
          """
          abs_path = os.path.normpath( os.path.abspath(path) )
-         self.config_path.insert(0, abs_path)
+         self._config_path.insert(0, abs_path)
 
      @classmethod
      def set_path(self, path):
@@ -97,15 +102,15 @@ class argconfig(argparse.Action):
          """
          if not isinstance(path, list):
              path = [path]
-         self.config_path = []
+         self._config_path = []
          for p in path:
              abs_path = os.path.normpath( os.path.abspath(p) )
-             self.config_path.append( abs_path )
+             self._config_path.append( abs_path )
 
      @classmethod
      def get_path(self):
          """Return configuration file paths."""
-         return self.config_path
+         return self._config_path
 
      # file class method
 
@@ -114,20 +119,20 @@ class argconfig(argparse.Action):
          """Set configuration file name.
             Overwrite default configuration file name.
          """
-         self.config_file = file_name
+         self._config_file = file_name
 
      @classmethod
      def get_file(self):
          """Return configuration file name."""
-         return self.config_file
+         return self._config_file
 
      @classmethod
      def get_file_list(self):
          """Return list of configuration files."""
          l = []
          # loop on config paths
-         for d in self.config_path:
-            file_cfg = os.path.join( d, self.config_file )
+         for d in self._config_path:
+            file_cfg = os.path.join( d, self._config_file )
             if os.path.isfile(file_cfg):
                l.append(file_cfg)
          return l
@@ -141,46 +146,10 @@ class argconfig(argparse.Action):
          """
          if not isinstance(section, list):
              section = [section]
-         self.config_section = section
+         self._config_section = section
 
      @classmethod
      def get_section(self):
          """Return section."""
-         return self.config_section
+         return self._config_section
 
-
-def main():
-
-   # parse command line options
-   parser = argparse.ArgumentParser(prog='argconfig',
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-   #action.set_path(['path1', 'path2'])
-   #action.add_path('path3')
-   #action.set_section(['sec1', 'sec2'])
-
-   argconfig.set_path('.')
-   argconfig.set_section('argconfig')
-
-   parser.add_argument('-a', dest = 'dest_a', help = 'help_a',
-                       action = argconfig)
-   parser.add_argument('-b', dest = 'dest_b', help = 'help_b', default = 'default_b',
-                       action = argconfig)
-   parser.add_argument('-c', dest = 'dest_c', help = 'help_c', default = 42, type=int,
-                       action = argconfig)
-
-   parser.add_argument('-x', dest = 'dest_x', help = 'help_x', default = 42, type=int,
-                       #section = 'argconfig',
-                       action = argconfig)
-
-   parser.add_argument('-d', dest = 'dest_d', help = 'help_d', default = 'a string',
-                       action = argconfig)
-
-   args = parser.parse_args()
-
-   print args
-
-
-
-if __name__ == "__main__":
-   main()
